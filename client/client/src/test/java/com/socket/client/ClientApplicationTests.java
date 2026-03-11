b315402a0ef35c558cf979608d1f00c937b7dcdc
ghghwldk <ghghwldk@gmail.com>
2026-03-11 21:01:10 +0900

    first commit

---
+++ client/client/src/test/java/com/socket/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socket.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
